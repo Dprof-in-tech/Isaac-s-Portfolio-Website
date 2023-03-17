a4ae3746974a85cae408d7c608c3b2bf6783f52f
cartwright <amaechiisaac450@gmail.com>
2023-03-17 03:05:12 +0100

    rewrote my portfolio website

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ body = document.getElementsByName('body')
 themeicon = document.querySelector('.themeicon')
 a = document.getElementsByTagName('a')
 
-function animationend(){
-    document.body.style.animationPlayState= 'paused';
-}
-function animationstart(){
-    document.body.style.animationPlayState= 'running';
-}
-function dark(){
-    document.body.style.backgroundColor= 'black';
-    document.body.style.color= 'white';
-}
 
-function light(){
-    document.body.style.backgroundColor= 'white';
-    document.body.style.color= 'black';
+function printSpan(intro, message, speed){
+    var i=0;
+    var interval = setInterval(function(){
+        document.getElementById(intro).innerHTML+=message.charAt(i);
+        i++;
+        if(i>message.length){
+            clearInterval(interval);
+        }
+    }, speed);
 }
+printSpan( "intro", "Hello, I am Cartwright.", 200);
+
+function printCon(about, message, speed){
+    var i=0;
+    var interval = setInterval(function(){
+        document.getElementById(about).innerHTML+=message.charAt(i);
+        i++;
+        if(i>message.length){
+            setTimeout(function(){
+                var j=message.length;
+                var deleteInterval = setInterval(function(){
+                    var newMessage = message.substring(0,j);
+                    document.getElementById(about).innerHTML=newMessage;
+                    j--;
+                    if(j<0){
+                        clearInterval(deleteInterval);
+                        printCon(about,message,speed);
+                    }
+                }, speed);
+            }, 0); // add delay before deleting text
+            clearInterval(interval);
+        }
+    }, speed);
+}
+printCon( "about", "I'm a DIGITAL MARKETER. ", 90);
+
+
+
